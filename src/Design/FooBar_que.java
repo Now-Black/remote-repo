9060e8a650fc60de87231006e3cb3f3ff42ac89a
black <1094855191@qq.com>
2025-08-18 17:17:43 +0800

    que

--- src/Design/FooBar_que.java
+++ src/Design/FooBar_que.java
@@ -1,8 +1,14 @@
 package Design;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 class FooBar_que {
     private int n;
-
+    private BlockingQueue<Integer> foo = new ArrayBlockingQueue<>(1);
+    private BlockingQueue<Integer> bar = new ArrayBlockingQueue<>(1);
     public FooBar_que(int n) {
         this.n = n;
     }
@@ -10,18 +16,20 @@ class FooBar_que {
     public void foo(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-
+            foo.put(i);
             // printFoo.run() outputs "foo". Do not change or remove this line.
             printFoo.run();
+            bar.put(i);
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-
+            foo.take();
             // printBar.run() outputs "bar". Do not change or remove this line.
             printBar.run();
+            bar.take();
         }
     }
 }
