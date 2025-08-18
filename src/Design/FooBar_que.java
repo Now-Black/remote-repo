de27da2805e5cc7e3b451f7c6ead3104186d2276
black <1094855191@qq.com>
2025-08-18 17:11:06 +0800

    que

---
+++ src/Design/FooBar_que.java
@@ -0,0 +1,27 @@
+package Design;
+
+class FooBar_que {
+    private int n;
+
+    public FooBar_que(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+        }
+    }
+}
