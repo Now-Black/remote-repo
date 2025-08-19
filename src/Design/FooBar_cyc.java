ae3eba58fce9dfc13a4afc7351c189917c0b0aaa
black <1094855191@qq.com>
2025-08-19 18:17:30 +0800

    cyc

--- src/Design/FooBar_cyc.java
+++ src/Design/FooBar_cyc.java
@@ -1,4 +1,43 @@
 package Design;
 
-public class FooBar_cyc {
+import java.util.concurrent.CyclicBarrier;
+
+class FooBar_cyc {
+    private int n;
+    CyclicBarrier cy = new CyclicBarrier(2);
+    volatile boolean tar = true;
+    public FooBar_cyc(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (!tar){
+                Thread.yield();
+            }
+
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            tar = false;
+            try {
+                cy.await();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            try {
+                cy.await();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+        }
+    }
 }
