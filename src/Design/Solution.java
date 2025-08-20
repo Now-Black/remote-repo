a763709faa5fb6617982923b372889fd1c256421
black <1094855191@qq.com>
2025-08-20 21:27:28 +0800

    FizzBuzz

---
+++ src/Design/Solution.java
@@ -0,0 +1,25 @@
+package Design;
+
+class Solution {
+    private
+
+
+    public Solution(int[] nums) {
+
+    }
+
+    public int[] reset() {
+
+    }
+
+    public int[] shuffle() {
+
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
