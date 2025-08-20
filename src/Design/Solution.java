d1ea0e262a159cf392408b0300a27963c7b2c99a
black <1094855191@qq.com>
2025-08-20 21:30:59 +0800

    FizzBuzz

--- src/Design/Solution.java
+++ src/Design/Solution.java
@@ -1,19 +1,30 @@
 package Design;
 
-class Solution {
-    private
+import java.util.Random;
 
+class Solution {
+    private final int[] nums;
+    private final Random random = new Random();
 
     public Solution(int[] nums) {
-
+        this.nums = nums;
     }
 
     public int[] reset() {
-
+        return this.nums;
     }
 
     public int[] shuffle() {
-
+        int[] temp = nums.clone();
+        for(int i = 0 ; i < temp.length; i++){
+            swap(temp,i,i+random.nextInt(temp.length-i));
+        }
+        return temp;
+    }
+    private void swap(int[] nums , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
 
