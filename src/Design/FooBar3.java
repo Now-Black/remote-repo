bc67c9c21dffef3f10f27850782f332f069fcf7f
black <1094855191@qq.com>
2025-08-21 21:09:49 +0800

    FizzBuzz

--- src/Design/FooBar3.java
+++ src/Design/FooBar3.java
@@ -17,7 +17,6 @@ class FooBar3 {
         CompletableFuture<Void> chain = CompletableFuture.completedFuture(null);
         /*创建一个已完成的起始点*/
         for(int i = 0 ; i < n ; i++){
-
             chain = chain.thenRunAsync(()->{foo.run();},executor);
             /*↑ 将 foo 任务追加到链的末尾*/
             chain = chain.thenRunAsync(()->{bar.run();},executor);
