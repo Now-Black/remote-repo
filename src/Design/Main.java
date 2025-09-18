c88abffcad5103544bc3e5604e1865b798790972
black <1094855191@qq.com>
2025-09-18 16:40:19 +0800

    minSubArrayLen1

--- src/Design/Main.java
+++ src/Design/Main.java
@@ -18,9 +18,17 @@ public class Main {
         c a = new c();
     }
     public int minSubArrayLen(int target, int[] nums) {
-
-
-
+        int sum = 0;
+        int index = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            sum += nums[i];
+            if(sum > target)return i-index+1;
+            if(sum < 0){
+                sum = 0;
+                index = i+1;
+            }
+        }
+        return 0;
     }
 
 }
