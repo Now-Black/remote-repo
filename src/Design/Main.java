8a1edac2f3d5d06de302d2f92902b50a5d3308d7
black <1094855191@qq.com>
2025-10-03 15:20:58 +0800

    minSubArrayLen1

--- src/Design/Main.java
+++ src/Design/Main.java
@@ -1,7 +1,14 @@
 package Design;
+import sun.misc.ConditionLock;
+
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     static class  c {
 
@@ -10,9 +17,25 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         List<? super Integer> list = new ArrayList<>();
+        int[] neum = new int[11];
+         int D =  neum.length;
 
-        c a = new c();
+        Set<Character> set = new HashSet<>();
+        StringBuilder s = null;
+        Random ran = new Random();
+        Map<Integer , Integer> map = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+//        map.merge(a,1,Integer::sum);
+        PriorityQueue<int[]> priorityQueue  = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        StringBuilder str = new StringBuilder();
+        str = s;
+        String ssss;
 
+        ReentrantLock lock = new ReentrantLock();
+        Semaphore semaphore = new Semaphore(1);
+        Arrays.copyOfRange()
+        priorityQueue.poll();
+        System.out.println((int)Math.sqrt(12));
     }
     public  void  a(){
         c a = new c();
@@ -20,16 +43,18 @@ public class Main {
     public int minSubArrayLen(int target, int[] nums) {
         int sum = 0;
         int index = 0;
+        int ans = Integer.MAX_VALUE;
         for(int i = 0 ; i < nums.length ; i++){
             sum += nums[i];
-            if(sum > target)return i-index+1;
-            if(sum < 0){
-                sum = 0;
-                index = i+1;
+            while (sum >= target){
+                ans = Math.min(ans , i-index + 1);
+                sum -= nums[index];
+                index++;
             }
         }
-        return 0;
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
+
 
 }
 
